13c9ae754ae41942e5370e99797c54e8260c0dd7
unknown <rajumca44@gmail.com>
2023-12-06 01:13:41 -0800

    My new react app

---
+++ App.js
@@ -0,0 +1,30 @@
+// Create HTML Tag from ReactJS(Heading)
+// By using React DOM I want create a root.
+// I want render the element to the 
+
+const heading = React.createElement("h1", {id: "my h1 tag"}, "Hello from ReactJS"); //Any HTML Element
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+
+// Deployment(Loval Environment) -  QA (Quality analisis) -  Production or live or Deployee
+
+
+// ReactJS we dont copy and paste
+
+// Minification, Bundleing, compress, optimization?
+
+// Build?
+
+// Web packages?
+
+// How to start a Applications?
+
+// We need to install a packages into our application.
+
+// NPM (Node package Manager)- Managing the packages.
+
+// Node JS - Install
+
+
